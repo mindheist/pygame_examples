79a37a529fc76882560d382aa25b41bbe3b40c86
MindHeist <dragonzsnake@gmail.com>
2014-10-18 21:41:57 -0700

    + Adding More Star Stats Data

--- seti/equilibrium.py
+++ seti/equilibrium.py
@@ -3,6 +3,8 @@
 import pygame
 import random
 import time
+import pygbutton
+import sys
 
 # DEFINE COLORS
 
@@ -38,11 +40,14 @@ def main():
 	background = pygame.image.load("kepler-dark.jpg").convert()
 	background.set_colorkey(WHITE)
 
-	alien1 = pygame.image.load("alien_1.jpg").convert()
-	alien2 = pygame.image.load("alien2.jpg").convert()
-	alien3 = pygame.image.load("alien3.jpg").convert()
-	alien4 = pygame.image.load("alien4.jpg").convert()
-	alien5 = pygame.image.load("alien5.jpeg").convert()
+	star1 = pygame.image.load("star1.jpg").convert()
+	star2 = pygame.image.load("star2.jpg").convert()
+	star3 = pygame.image.load("star3.jpg").convert()
+	star4 = pygame.image.load("star4.jpg").convert()
+	star5 = pygame.image.load("star5.jpg").convert()
+
+	alien1 = pygame.image.load("alien1.jpg").convert()
+
 	catastrophes_sprite = pygame.image.load("catastrophes.png").convert()
 
 
@@ -70,18 +75,24 @@ def main():
 	textRect4.x = 4
 	textRect4.y = 73
 
+	buttonWhiteBg = pygbutton.PygButton((150, 50, 60, 30), 'White')
+	buttonRedBg = pygbutton.PygButton((150, 100, 60, 30), 'Red')
+	buttonGreenBg = pygbutton.PygButton((150, 150, 60, 30), 'Green')
+	allbuttons = (buttonWhiteBg, buttonRedBg, buttonGreenBg)
+
+
 
 	star_list = pygame.sprite.Group()
 
 	all_sprites_list = pygame.sprite.Group()
      
-	star =Star ( GREEN,5,5)
+	star =Star ( GREEN, 7, 7)
 	star.rect.x = 750
 	star.rect.y = 250
 	star_list.add(star)
 	all_sprites_list.add(star)
 
-	star = Star(GREEN, 5, 5)
+	star = Star(GREEN, 7, 7)
 	star.rect.x = 500
 	star.rect.y = 200
 	star_list.add(star)
@@ -129,13 +140,24 @@ def main():
 
 	score = 0
 
+# === Step 1 : 
+
 	while not done :
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				done = True
 
+			buttonBgColor = None
+			if 'click' in buttonWhiteBg.handleEvent(event):
+				buttonBgColor = WHITE
+			if 'click' in buttonRedBg.handleEvent(event):
+				buttonBgColor = RED
+			if 'click' in buttonGreenBg.handleEvent(event):
+				buttonBgColor = GREEN
+
 			if event.type == pygame.MOUSEBUTTONUP:
 				pos = pygame.mouse.get_pos()
+				print pos
 				clicked_sprite = [s for s in star_list if s.rect.collidepoint(pos)]
 				print clicked_sprite 
 				for s in clicked_sprite:
@@ -149,7 +171,8 @@ def main():
 						screen.blit(text4,textRect4)
 						screen.blit(catastrophes_sprite,(110,10))
 						all_sprites_list.draw(screen)
-						screen.blit(alien1,(1150,1))
+						screen.blit(star1,(1150,1))
+
 
 					elif s.rect.x ==500 and s.rect.y ==200 :
 						screen.blit(background,(0,0))
@@ -159,7 +182,7 @@ def main():
 						screen.blit(text4,textRect4)
 						screen.blit(catastrophes_sprite,(110,10))
 						all_sprites_list.draw(screen)
-						screen.blit(alien2,(1200,1))
+						screen.blit(star2,(1150,1))
 
 					elif s.rect.x ==850 and s.rect.y ==300 :
 						screen.blit(background,(0,0))
@@ -169,7 +192,7 @@ def main():
 						screen.blit(text4,textRect4)
 						screen.blit(catastrophes_sprite,(110,10))
 						all_sprites_list.draw(screen)
-						screen.blit(alien3,(1200,1))
+						screen.blit(star3,(1150,1))
 
 					elif s.rect.x ==750 and s.rect.y ==350 :
 						screen.blit(background,(0,0))
@@ -179,7 +202,7 @@ def main():
 						screen.blit(text4,textRect4)
 						screen.blit(catastrophes_sprite,(110,10))
 						all_sprites_list.draw(screen)
-						screen.blit(alien4,(1200,1))
+						screen.blit(star4,(1150,1))
 
 					elif s.rect.x ==600 and s.rect.y ==500 :
 						screen.blit(background,(0,0))
@@ -189,7 +212,7 @@ def main():
 						screen.blit(text4,textRect4)
 						screen.blit(catastrophes_sprite,(110,10))
 						all_sprites_list.draw(screen)
-						screen.blit(alien5,(1200,1))
+						screen.blit(star5,(1150,1))
 
 					elif s.rect.x != 0 and s.rect.y != 0:
 						screen.blit(background,(0,0))
@@ -202,6 +225,20 @@ def main():
 						pygame.draw.rect(screen,WHITE, (1150,1,350,100))
 						pygame.draw.rect(screen,GREEN, (1150,101,350,300))
 						pygame.draw.rect(screen,MAROON,(1150,401,350,100))
+
+				if (pos[0] >= 1189 and pos[0] <= 1289):
+					screen.blit(background,(0,0))
+					screen.blit(text,textRect)
+					screen.blit(text2,textRect2)
+					screen.blit(text3,textRect3)
+					screen.blit(text4,textRect4)
+					screen.blit(catastrophes_sprite,(110,10))
+					all_sprites_list.draw(screen)
+					screen.blit(star1,(1150,1))
+					screen.blit(alien1,(1150,520))
+
+
+
 
 		clock.tick(60)
 
