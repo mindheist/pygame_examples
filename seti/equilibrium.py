99e4ae47d4c7e34573e9fc087ef9dd1c95e64b1a
MindHeist <dragonzsnake@gmail.com>
2014-10-18 19:31:57 -0700

    + Added a new Alien file

--- seti/equilibrium.py
+++ seti/equilibrium.py
@@ -6,12 +6,12 @@ import time
 
 # DEFINE COLORS
 
-BLACK = ( 0,  0,  0)
-WHITE = (255,255,255)
-RED   = (255,  0,  0)
-GREEN = (  0,255,  0)
-BLUE  = (  0,  0,255)
-MAROON = (128,0,0)
+BLACK  = (  0,  0,  0)
+WHITE  = (255,255,255)
+RED    = (255,  0,  0)
+GREEN  = (  0,255,  0)
+BLUE   = (  0,  0,255)
+MAROON = (128,  0,  0)
 
 # SCREEN SIZE
 
